113b8d2d2845979207833fc3ab6f31c7840af2b4
Slava Tsoy <emonsipurewerifaine@yandex.com>
2023-02-02 02:07:18 +0300

    sdqwe

--- PCRepair/Models/MyRequest.cs
+++ PCRepair/Models/MyRequest.cs
@@ -10,10 +10,15 @@ namespace PCRepair.Models
         public int ID { get; set; }
         public DateTime CreateDate { get; set; }
         public string ClientName { get; set; }
+        public string ClientSurName { get; set; }
+        public string ClientSurSurName { get; set; }
+
         public List<string> PCConfig { get; set; }
         public string PCConfigselected { get; set; }
 
-        public string Malfunction { get; set; }
+        public List<string> Malfunction { get; set; }
+        public string Malfunctionselected { get; set; }
+
         public DateTime CloseDate { get; set; }
         public string RepairActions { get; set; }
         public string RepairCost { get; set; }
@@ -22,6 +27,14 @@ namespace PCRepair.Models
 
         public MyRequest()
         {
+            Malfunction = new List<string>()
+            {
+                "Намок",
+                "Сломался ЖД",
+                "Сломался Монитор",
+                "Сломались колонки",
+                "Компьютер просто не включается"
+            };
             PCConfig = new List<string>()
             {
                 "Desktop",
